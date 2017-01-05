2aeb4b4ff6e1829a15b76dcef06380f44b245d8a
ClanMax <me@clanmax.ru>
2017-01-05 21:43:57 +0300

    added picture to post.

--- anchorcms-theme/article.php
+++ anchorcms-theme/article.php
@@ -3,6 +3,9 @@
 <article class="posttext">
     <div class="container">
          <h3 class="posttext_h3"><?php echo article_title(); ?></h3>
+         <?php if(article_custom_field('post_image', '')!=""): ?>
+            <img src="<?php echo article_custom_field('post_image', ''); ?>" alt=""  class="posttext_image">
+            <?php endif; ?>
           <div class="posttext_p"><?php echo article_markdown(); ?></div>
     </div>
 </article>
