e6d1870f1d40e742fd968f8dc10557795ce1c608
ClanMax <me@clanmax.ru>
2017-01-05 22:15:36 +0300

    fixed bug with responsive image

--- anchorcms-theme/posts.php
+++ anchorcms-theme/posts.php
@@ -5,10 +5,10 @@
 <?php if(has_posts()): ?> 
 <?php $i = 0; while(posts()): $i++; ?>
 <a href="<?php echo article_url(); ?>">
-    <article class="post">
        <?php if(article_custom_field('post_image', '')!=""): ?>
             <img src="<?php echo article_custom_field('post_image', ''); ?>" alt=""  class="post_image">
         <?php endif; ?>
+    <article class="post">
         <div class="post_about">
             <h1 class="post_topic"><?php echo article_title(); ?></h1>
             <time class="post_date"><?php echo relative_time(article_time()); ?></time>
