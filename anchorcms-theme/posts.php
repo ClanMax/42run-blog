455c8fc53d0ffa97ab32dad9c5eff1b8c2965ae6
ClanMax <me@clanmax.ru>
2017-01-07 19:25:33 +0300

    rebuild anchor design

--- anchorcms-theme/posts.php
+++ anchorcms-theme/posts.php
@@ -4,17 +4,18 @@
 
 <?php if(has_posts()): ?> 
 <?php $i = 0; while(posts()): $i++; ?>
-<a href="<?php echo article_url(); ?>">
-       <?php if(article_custom_field('post_image', '')!=""): ?>
+        <article class="post">
+        <div class="container">
+           <a href="<?php echo article_url(); ?>"><h3 class="post_topic"><?php echo article_title(); ?></h3></a>
+            <?php if(article_custom_field('post_image', '')!=""): ?>
             <img src="<?php echo article_custom_field('post_image', ''); ?>" alt=""  class="post_image">
         <?php endif; ?>
-    <article class="post">
-        <div class="post_about">
-            <h1 class="post_topic"><?php echo article_title(); ?></h1>
-            <time class="post_date"><?php echo relative_time(article_time()); ?></time>
+             
+        <div class="posttext_p"><?php echo article_markdown(); ?></div>
+              
+        <time class="post_date"><?php echo relative_time(article_time()); ?></time>
         </div>
     </article>
-    </a>
 <?php endwhile; ?>
 <?php else: ?>
 		<p>There are no posts - get to it!</p>
